325cb1278f3f9a9ece248ec60339bcfd4072fdea
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-01-30 12:36:26 +0530

    git push

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./todoList";
+import LoginForm from "./loginForm";
+import "./App.css";
+// const port = 8080
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState(undefined);
+  const [userName, setUserName] = useState(undefined);
+
+  const getUserName = () => {
+    return fetch('https://firsttodo-app.herokuapp.com/userinfo', { credentials: "include"})
+    .then(r => {
+      if(r.ok) {
+        return r.json();
+      } else {
+        setLoggedIn(false);
+        setUserName(undefined);
+        return { success: false };
+      }
+    }).then(r => {
+      if(r.success !== false) {
+        setLoggedIn(true);
+        setUserName(r.userName);
+      }
+    });
+  }
+
+  useEffect(() => {
+    getUserName();
+  }, []);
+
+
+  const signupHandler = (username, password) => {
+    loginOrSignup('https://firsttodo-app.herokuapp.com/signup', username, password);
+  };
+  const loginHandler = (username, password) => {
+    loginOrSignup('https://firsttodo-app.herokuapp.com/login', username, password);
+  };
+
+  const logoutHandler = () => {
+    return fetch('https://firsttodo-app.herokuapp.com/logout', { credentials: 'include'})
+    .then(r => {
+      if(r.ok) {
+        setLoggedIn(false);
+        setUserName(undefined);
+      }
+    })
+  };
+
+
+  const loginOrSignup = (url, username, password) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ userName: username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials:"include"
+    })
+      .then((r) => {
+        if(r.ok) {
+          return { success: true };
+        } else {
+          return r.json()
+        }
+      })
+      .then((r) => {
+        if(r.success === true) {
+          return getUserName();
+        } else {
+          setError(r.err);
+        }
+      });
+  }
+  return loggedIn ? ( <TodoList username={userName} logoutHandler={logoutHandler}/>
+  ) : (<LoginForm signupHandler={signupHandler} loginHandler={loginHandler} error={error}/>
+  );
+}
+//App.listen(port, () => console.log(`App listening on port ${port}!`))
+export default App;
+
+
+// react-scripts start
